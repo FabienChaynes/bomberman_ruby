b19879582578f427f0fff98f749c4562314990b7
Fabien Chaynes <fabien.chaynes@ringcentral.com>
2023-05-14 00:15:10 +0200

    Show window

--- lib/bomberman_ruby.rb
+++ lib/bomberman_ruby.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "bomberman_ruby/version"
+require_relative "bomberman_ruby/window"
 
 module BombermanRuby
   class Error < StandardError; end
   # Your code goes here...
 end
+
+BombermanRuby::Window.new.show
---
+++ lib/bomberman_ruby/window.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "gosu"
+
+module BombermanRuby
+  class Window < Gosu::Window
+    WIDTH = 240
+    HEIGHT = 160
+
+    def initialize
+      super(WIDTH, HEIGHT)
+    end
+
+    def update
+    end
+
+    def draw
+    end
+  end
+end
