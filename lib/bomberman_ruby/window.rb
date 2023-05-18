d737dfe1ccd0c14e916ebddf0c98d728069c8dc3
Fabien Chaynes <fabien.chaynes@ringcentral.com>
2023-05-18 10:44:58 +0200

    Handle window resizing

--- lib/bomberman_ruby/window.rb
+++ lib/bomberman_ruby/window.rb
@@ -5,21 +5,30 @@ module BombermanRuby
     WIDTH = 240
     HEIGHT = 160
     SPRITE_SIZE = 16
-    SCALING_FACTOR = 3
+    INITIAL_SCALING_FACTOR = 3
 
     def initialize
-      super((WIDTH * SCALING_FACTOR).round, (HEIGHT * SCALING_FACTOR).round)
+      super((WIDTH * INITIAL_SCALING_FACTOR).round, (HEIGHT * INITIAL_SCALING_FACTOR).round)
       @game = Game.new
     end
 
     def update
+      self.resizable = true
       @game.update
     end
 
     def draw
-      Gosu.scale(SCALING_FACTOR, SCALING_FACTOR) do
+      Gosu.scale(scaling_factor, scaling_factor) do
         @game.draw
       end
+    end
+
+    private
+
+    def scaling_factor
+      width_scaling_factor = width / WIDTH.to_f
+      height_scaling_factor = height / HEIGHT.to_f
+      [width_scaling_factor, height_scaling_factor].min
     end
   end
 end
